8a540f9ec0ba7b4cace5d6ad7e52e9ffbff492ca
Daniel Pereira Sandino <danpersan390@gmail.com>
2026-05-12 14:40:01 -0600

    Update toma_datos.py

--- toma_datos.py
+++ toma_datos.py
@@ -1526,7 +1526,7 @@ try:
     # -----------------------------------------------------------------
     _ronda += 1
 
-    graficar_estado_actual(DATA_CONV_FOLDER, VIVIENDAS)
+    
 
     # -----------------------------------------------------------------
     # EXPORTACIÓN A GEOJSON POR CADA MEDIDOR UNIFICADO
